cd2027ff378f6c72443a723bceae7506a404f18d
MarX Decentralized Exchange Deployer <96611603+marx-dex-deployer@users.noreply.github.com>
2021-12-30 01:55:04 +0200

    MarXDEX contracts

--- test/GovernorAlpha.spec.ts
+++ test/GovernorAlpha.spec.ts
@@ -18,19 +18,19 @@ describe('GovernorAlpha', () => {
   const [wallet] = provider.getWallets()
   const loadFixture = createFixtureLoader([wallet], provider)
 
-  let uni: Contract
+  let marx: Contract
   let timelock: Contract
   let governorAlpha: Contract
   beforeEach(async () => {
     const fixture = await loadFixture(governanceFixture)
-    uni = fixture.uni
+    marx = fixture.marx
     timelock = fixture.timelock
     governorAlpha = fixture.governorAlpha
   })
 
-  it('uni', async () => {
-    const balance = await uni.balanceOf(wallet.address)
-    const totalSupply = await uni.totalSupply()
+  it('marx', async () => {
+    const balance = await marx.balanceOf(wallet.address)
+    const totalSupply = await marx.totalSupply()
     expect(balance).to.be.eq(totalSupply)
   })
 
@@ -48,7 +48,7 @@ describe('GovernorAlpha', () => {
     expect(votingPeriod).to.be.eq(40320)
     const timelockAddress = await governorAlpha.timelock()
     expect(timelockAddress).to.be.eq(timelock.address)
-    const uniFromGovernor = await governorAlpha.uni()
-    expect(uniFromGovernor).to.be.eq(uni.address)
+    const marxFromGovernor = await governorAlpha.marx()
+    expect(marxFromGovernor).to.be.eq(marx.address)
   })
 })
--- test/scenarios/setFeeTo.spec.ts
+++ test/scenarios/setFeeTo.spec.ts
@@ -2,7 +2,7 @@ import chai, { expect } from 'chai'
 import { Contract, BigNumber, utils } from 'ethers'
 import { solidity, MockProvider, createFixtureLoader, deployContract } from 'ethereum-waffle'
 
-import UniswapV2Factory from '@uniswap/v2-core/build/UniswapV2Factory.json'
+import MarXDEXV2Factory from '@marx-dex/marx-dex-v2-core/build/MarXDEXV2Factory.json'
 
 import { governanceFixture } from '../fixtures'
 import { mineBlock, DELAY } from '../utils'
@@ -20,19 +20,19 @@ describe('scenario:setFeeTo', () => {
   const [wallet] = provider.getWallets()
   const loadFixture = createFixtureLoader([wallet], provider)
 
-  let uni: Contract
+  let marx: Contract
   let timelock: Contract
   let governorAlpha: Contract
   beforeEach(async () => {
     const fixture = await loadFixture(governanceFixture)
-    uni = fixture.uni
+    marx = fixture.marx
     timelock = fixture.timelock
     governorAlpha = fixture.governorAlpha
   })
 
   let factory: Contract
-  beforeEach('deploy uniswap v2', async () => {
-    factory = await deployContract(wallet, UniswapV2Factory, [timelock.address])
+  beforeEach('deploy MarXDEX v2', async () => {
+    factory = await deployContract(wallet, MarXDEXV2Factory, [timelock.address])
   })
 
   it('setFeeTo', async () => {
@@ -40,10 +40,10 @@ describe('scenario:setFeeTo', () => {
     const value = 0
     const signature = 'setFeeTo(address)'
     const calldata = utils.defaultAbiCoder.encode(['address'], [timelock.address])
-    const description = 'Set feeTo on the UniswapV2Factory to the timelock address.'
+    const description = 'Set feeTo on the MarXDEXV2Factory to the timelock address.'
 
     // activate balances
-    await uni.delegate(wallet.address)
+    await marx.delegate(wallet.address)
     const { timestamp: now } = await provider.getBlock('latest')
     await mineBlock(provider, now)
 
